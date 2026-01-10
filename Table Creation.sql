e919bacda8fd50e44f938b5232abc0928212fb6e
Yaswanth Kalis <kalisyaswanth@gmail.com>
2026-01-10 13:06:11 +0530

    Creating a Table

---
+++ Table Creation.sql
@@ -0,0 +1,7 @@
+CREATE TABLE AUTHOR2 (
+    author_id CHAR(2) PRIMARY KEY NOT NULL,
+    firstname VARCHAR(20) NOT NULL,
+    lastname VARCHAR(20) NOT NULL,
+    email_id VARCHAR(30),
+    COUNTRY CHAR(2)
+);
