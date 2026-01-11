fe04ddab91991f7c1b950692b9c288b69f8332cb
Yaswanth Kalis <kalisyaswanth@gmail.com>
2026-01-11 12:21:09 +0530

    To insert values into the table

---
+++ Value Insertion.sql
@@ -0,0 +1,7 @@
+INSERT INTO AUTHOR --table name
+    (author_id,firstname,lastname,email_id,country)
+VALUES
+    ('A1','Yaswanth','Kalis','yk@gmail.com','IN'),
+    ('A2','Ash','Ketchum','ak@yahoo.com','US'),
+    ('A3','XXX','YYY','xy@gmail.com','CA'),
+    ('A4','Harry','Potter','hp@yahoo.com','EN');
