60956c310a28d63d9329815291ff9e35f75e3eb4
Yaswanth Kalis <kalisyaswanth@gmail.com>
2026-01-13 10:27:54 +0530

    Adding SQL commands for update, delete, and drop records

---
+++ DELETE , UPDATE , DROP.sql
@@ -0,0 +1,9 @@
+UPDATE AUTHOR
+SET country='JP'
+WHERE author_id='A2';
+
+DELETE FROM AUTHOR
+WHERE author_id = 'A2'; --Deletes particular row
+
+DROP TABLE table_name; -- Deletes the entire table
+
