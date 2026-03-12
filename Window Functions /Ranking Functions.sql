aa923c5c181a0a922469f90df9fc8a808d0651a9
Yaswanth Kalis <kalisyaswanth@gmail.com>
2026-03-12 19:45:42 +0530

    r

--- Window Functions /Ranking Functions.sql
+++ Window Functions /Ranking Functions.sql
@@ -1,4 +1,4 @@
 ROW_NUMBER()	-   Assigns a unique sequential number to each row
-RANK()	      -   Assigns rank with gaps if there are ties
+RANK()	       -   Assigns rank with gaps if there are ties
 DENSE_RANK()	-   Assigns rank without gaps
 NTILE(n)	    -   Divides rows into n equal groups
